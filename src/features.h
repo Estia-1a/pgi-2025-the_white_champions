4f9d702fb97bf7ae350daeee28d11fc63c850383
Kentin-hub <kentin.ribeirodejesus@etu.estia.fr>
2025-05-16 17:08:42 +0200

    Ajout Dimension, Color of the first pixel , Color of the 10th pixel

--- src/features.h
+++ src/features.h
@@ -1,7 +1,11 @@
 #ifndef FEATURES_H
 #define FEATURES_H
+#include <stdio.h>
 
 
 void helloWorld();
-
+void dimension (char *source_path);
+void first_pixel (char *source_path);
+void tenth_pixel (char *source_path);
+void second_line(char *source_path);
 #endif
--- src/main.c
+++ src/main.c
@@ -34,6 +34,24 @@ int main(int argc, char **argv) {
   /*
    * TO COMPLETE
    */
-  
+  if ( strncmp( configuration.command, "dimension", 9 ) == 0 ) {
+    /* dimension() function is defined in feature.h and implemented in feature.c */
+    dimension( configuration.filenames[0] );
+}
+  if ( strncmp( configuration.command, "first_pixel", 11 ) == 0 ) {
+    /* first_pixel() function is defined in feature.h and implemented in feature.c */
+    first_pixel(configuration.filenames[0]);
+}
+
+  if ( strncmp( configuration.command, "tenth_pixel", 11 ) == 0 ) {
+    /* first_pixel() function is defined in feature.h and implemented in feature.c */
+    tenth_pixel(configuration.filenames[0]);
+}
+
+  if ( strncmp( configuration.command, "second_line", 11 ) == 0 ) {
+    /* first_pixel() function is defined in feature.h and implemented in feature.c */
+    tenth_pixel(configuration.filenames[0]);
+}
+
   return 0;
 }
